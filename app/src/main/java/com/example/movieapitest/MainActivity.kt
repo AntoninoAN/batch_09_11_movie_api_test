c9c9b2fcd4d6636cf49dee85b66438d081dd04d2
Antonino AN <andonino@gmail.com>
2020-10-06 11:17:50 -0400

    Add to backstack

--- app/src/main/java/com/example/movieapitest/MainActivity.kt
+++ app/src/main/java/com/example/movieapitest/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container,
                 FragmentDetail.createFragmentDetail(data))
+            .addToBackStack(null)
             .commit()
     }
 
